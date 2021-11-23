3c10a2c68b51f48687ec8cb7b0e9f30c45c73255
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2021-11-24 00:13:23 +0530

    Update phone_number_detail.py

--- phone_number_detail.py
+++ phone_number_detail.py
@@ -1,8 +1,8 @@
 import phonenumbers
 from phonenumbers import carrier, geocoder, timezone
 
-mobileNo = input("Mobile no. with country code:")
-mobileNo= phonenumbers.parse(mobileNo)
+mobileNo = input("Mobile no. with country code:")  #asking to enter mobile number
+mobileNo= phonenumbers.parse(mobileNo) # searching details of mobile number
 print(timezone.time_zones_for_number(mobileNo))
 print("Valid mobile number.", phonenumbers.is_valid_number(mobileNo))
 print("Checking possibility of Number:", phonenumbers.is_possible_number(mobileNo))
